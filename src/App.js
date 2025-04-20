764024d11b0722f7f1e1e386361848227a58d539
lavanya-0406 <lavanya04061999@gmail.com>
2025-04-21 03:54:40 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Blog from "./blog";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import "./App.css";
+function App() {
+  return <Blog />;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import Blog from "./blog";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error(
+    "Root element not found. Ensure the element with id='root' exists in index.html."
+  );
+}
